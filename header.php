7696246c25f6f9c1dc3a9570b36da4a57747227c
Gabriel Dzul <gabriel@yellowme.mx>
2020-01-22 14:41:06 -0600

    Added header

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+
+<head>
+
+  <title><?php wp_title(); ?></title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="Erilisdesign.com">
+
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/utilities.css">
+
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/custom.css">
+
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/fontawesome/all.min.css" type="text/css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/themify-icons.css" type="text/css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/animate.min.css">
+  <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/assets/css/vegas.min.css">
+
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i%7CBarlow:100,200,300,400,500,700">
+
+  <link rel="shortcut icon" href="<?= get_template_directory_uri() ?>/assets/images/favicon.ico">
+
+  <script src="<?= get_template_directory_uri() ?>/assets/js/modernizr-custom.js"></script>
+  <?php wp_head(); ?>
+
+</head>
+
+<body class="slideshow-background">
