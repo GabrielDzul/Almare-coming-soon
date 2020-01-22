8e58aa9e30de0ae1531515a123f8f425d24ed830
Gabriel Dzul <gabriel@yellowme.mx>
2020-01-22 14:40:44 -0600

    Added footer

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!-- Site footer -->
+<footer class="site-footer bg-light">
+  <?php wp_footer(); ?>
+  <div class="container">
+    <nav class="mb-3">
+      <ul class="list-inline text-center">
+        <li class="list-inline-item">
+          <a class="btn btn-sm btn-icon btn-outline-dark btn-transparent rounded-circle" href="#">
+            <span class="btn-icon-inner fab fa-facebook-f"></span>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn btn-sm btn-icon btn-outline-dark btn-transparent rounded-circle" href="#">
+            <span class="btn-icon-inner fab fa-instagram"></span>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn btn-sm btn-icon btn-outline-dark btn-transparent rounded-circle" href="#">
+            <span class="btn-icon-inner fab fa-twitter"></span>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn btn-sm btn-icon btn-outline-dark btn-transparent rounded-circle" href="#">
+            <span class="btn-icon-inner fab fa-pinterest"></span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <p class="text-center">© 2020 Almaré - All Rights Reserved</p>
+  </div>
+</footer>
+
+</body>
+
+</html>
